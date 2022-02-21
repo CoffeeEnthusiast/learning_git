77ce65b4a77ab107931f603a6e8769e9fc9a271f
Janos <bevcjan10@gmail.com>
2022-02-21 18:00:57 +0100

    Added some things

--- learning_git/PythonFile1.py
+++ learning_git/PythonFile1.py
@@ -1,1 +1,13 @@
-print("We printin'")
+# Time package
+import time
+# time delay of 5 seconds
+time.sleep(5)
+# Creating a list
+Languages = ["Slovenian", "Italian", "Spanish"]
+# Print statement
+print(Languages)
+
+for lan in Languages:
+    time.sleep(3)
+# After every 3 seconds an item from the list will be displayed
+    print(lan)
